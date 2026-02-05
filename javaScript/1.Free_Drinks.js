2136bb8973db7b83180380523f1a74486199dac0
kamalcodezen <kamaluddin7908@gmail.com>
2026-02-05 23:39:54 +0530

    task 1 complete

---
+++ javaScript/1.Free_Drinks.js
@@ -0,0 +1,12 @@
+/***
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+let burger = 500;
+if (burger > 500) {
+    console.log("you have free coke");
+} else {
+    console.log("you pay 30tk")
+}
